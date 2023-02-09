4daa6e251c7fa1e31c24f60539ade0395a75f447
bunty6377 <119603476+bunty6377@users.noreply.github.com>
2023-02-10 01:28:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+
+
+
+
+var length = document.querySelectorAll(".drum").length;
+for(var i = 0;i<length;i++){
+document.querySelectorAll("button")[i].addEventListener("click",function(){
+
+  var buttonInnerHTML = this.innerHTML;
+  makeSound(buttonInnerHTML);
+  buttonAnimation(buttonInnerHTML);
+
+});
+}
+
+
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+
+
+
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+    break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+    break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+    break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+    break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+    break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+    break;
+
+    case "l":
+      var kickBass = new Audio("sounds/kick-bass.mp3");
+      kickBass.play();
+    break;
+
+
+
+  
+    default: console.log(key);
+      
+  }
+
+
+}
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  } ,100 );
+
+}
+
+// // CALCULATOR MAKING
+// function add(num1,num2){
+//   return num1+num2;
+// }
+// function multiply(num1,num2){
+//   return num1*num2;
+// }
+// function substraction(num1,num2){
+//   return num1-num2;
+// }
+// function divide(num1,num2){
+//   return num1/num2;
+// }
+// function calculator(num1,num2,oprator){
+//   return oprator(num1,num2);
+// }
+// calculator(4,5,divide);
+//  //CALCULATOR
+
+
+
+
+
+  
+
+
